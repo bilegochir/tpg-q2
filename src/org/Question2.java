b0d5efdc373ff98e50f06f143cee6325f25ae4df
Bileg-Ochir <oxgetr@gmail.com>
2017-07-27 09:57:23 +1000

    try catch block added

--- src/org/Question2.java
+++ src/org/Question2.java
@@ -5,19 +5,26 @@ import java.util.Scanner;
 
 public class Question2 {
 
+	private static Scanner scan;
+
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Please insert your comma separated integers.");
-		String str = scan.nextLine();
+		try {
+			scan = new Scanner(System.in);
+			System.out.println("Please insert your comma separated integers");
+			String str = scan.nextLine();
 
-		int[] numbers = Arrays.stream(str.split(",")).mapToInt(Integer::parseInt).toArray();
-		Arrays.sort(numbers);
+			int[] numbers = Arrays.stream(str.split(",")).mapToInt(Integer::parseInt).toArray();
+			Arrays.sort(numbers);
 
-		if (numbers.length % 2 == 1) {
-			System.out.println(numbers[numbers.length / 2]);
-		} else {
-			double result = (numbers[numbers.length / 2 - 1] + numbers[numbers.length / 2]) / 2.0;
-			System.out.println(result);
+			if (numbers.length % 2 == 1) {
+				System.out.println(numbers[numbers.length / 2]);
+			} else {
+				double result = (numbers[numbers.length / 2 - 1] + numbers[numbers.length / 2]) / 2.0;
+				System.out.println(result);
+			}
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
 		}
+
 	}
 }
